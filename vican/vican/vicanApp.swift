d0d8dcc3fcea6db35ea364fb5297b42b604dd235
m00nbek <melikulov990@gmail.com>
2023-12-07 11:45:46 +0500

    init xcode proj

---
+++ vican/vican/vicanApp.swift
@@ -0,0 +1,17 @@
+//
+//  vicanApp.swift
+//  vican
+//
+//  Created by m00nbek on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct vicanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

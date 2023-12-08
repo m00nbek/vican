a0af6518ec4198074dead3eccee88df11e5adc6e
m00nbek <melikulov990@gmail.com>
2023-12-08 17:13:29 +0500

    create Home view

---
+++ vican/vican/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  vican
+//
+//  Created by m00nbek on 12/7/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            
+            NavigationLink(destination: Text("Forgot Password?")) {
+                Text("Forgot Password?")
+                    .foregroundColor(.blue)
+                    .padding(.top, 10)
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
--- vican/vican/vicanApp.swift
+++ vican/vican/vicanApp.swift
@@ -11,7 +11,11 @@ import SwiftUI
 struct vicanApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if AuthManager.shared.isAuthorized {
+                HomeView()
+            }
+            
+            LoginView()
         }
     }
 }

0d547b720b5217fafddc871b271a80727fbcc828
m00nbek <melikulov990@gmail.com>
2023-12-20 23:53:06 +0500

    [Workspaces] sample workspace data

--- vican/vican/Modules/Workspace/Models/Workspace.swift
+++ vican/vican/Modules/Workspace/Models/Workspace.swift
@@ -17,8 +17,8 @@ struct Workspace: Identifiable {
 extension Workspace {
     static var sample: [Workspace] {
         [
-            Workspace(name: "Workspace 1", description: "Description for Workspace 1", image: Image(systemName: "building.fill")),
-            Workspace(name: "Workspace 2", description: "Description for Workspace 2", image: Image(systemName: "building")),
+            Workspace(name: "Workspace 1", description: "Description for Workspace 1", image: Image(.workspace1)),
+            Workspace(name: "Workspace 2", description: "Description for Workspace 2", image: Image(.workspace2)),
             Workspace(name: "Workspace 3", description: "Description for Workspace 3", image: Image(systemName: "building.2"))
         ]
     }
